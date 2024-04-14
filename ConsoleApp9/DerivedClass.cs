e3181495d8aee2e56db3e3df8fb249ffa83480d7
stacy2906 <anastasiya.tkachyk.290606@gmail.com>
2024-04-14 19:23:00 +0300

    add program.cs

---
+++ ConsoleApp9/DerivedClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    internal class DerivedClass : IInterface3
+    {
+        public void Method1()
+        {
+            Console.WriteLine("Метод Method1");
+        }
+
+        public void Method2()
+        {
+            Console.WriteLine("Метод Method2");
+        }
+
+        public void Method3()
+        {
+            Console.WriteLine("Метод Method3");
+        }
+    }
+}
---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                // Создание объекта DerivedClass и вызов методов
+                DerivedClass dClass = new DerivedClass();
+                dClass.Method1();
+                dClass.Method2();
+                dClass.Method3();
+                Console.ReadKey();
+            
+        }
+    }
+}
